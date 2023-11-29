16208c24890ce534a3d469441aede4aceff4e35a
barnesgr123 <g.barnes@ucl.ac.uk>
2023-11-29 14:36:08 +0000

    bem changed to add many layers

--- tt_fwd_bem3.m
+++ tt_fwd_bem3.m
@@ -6,19 +6,24 @@ if ~isfield(S,'posunits'); error('please specify the current positiosn units!');
 if ~isfield(S,'ori'); S.ori = []; end
 if ~isfield(S,'sensors'); error('please specify the sensor structure!'); end
 if ~isfield(S,'T'); S.T = eye(4); end
+if ~isfield(S,'names'); S.names = {'blood','lungs','torso'}; end;
+if ~isfield(S,'ci'); S.ci = [.62 .05 .23]; end
+if ~isfield(S,'co'); S.co = [.23 .23  0 ]; end
 
 
 if isempty(which('hbf_BEMOperatorsPhi_LC'));
     tt_add_bem;
 end
 
-meshes = tt_load_meshes(S.T);
+meshes = tt_load_meshes_grb(S.T,S.names);
 [~, sf] = tt_determine_mesh_units(meshes);
 bmeshes = {};
 
+figure; hold on;
 for ii = 1:numel(meshes)
     bmeshes{ii}.p = meshes{ii}.vertices/sf; % conform to meters
     bmeshes{ii}.e = meshes{ii}.faces;
+    plot3(bmeshes{ii}.p(:,1),bmeshes{ii}.p(:,2),bmeshes{ii}.p(:,3),'m.')
 end
 
 % get sensors into meters 
@@ -37,9 +42,13 @@ coils = [];
 coils.p = S.sensors.coilpos;
 coils.n = S.sensors.coilori;
 
+plot3(src.pos(:,1),src.pos(:,2),src.pos(:,3),'g*')
+plot3(coils.p(:,1),coils.p(:,2),coils.p(:,3),'ko')
+
 % conductivity for, cardiac blood, lungs, thorax
-ci = [.62 .05 .23];
-co = [.23 .23 0];
+ci = S.ci;
+co = S.co;
+
 
 % Generate transfer matrix
 D = hbf_BEMOperatorsPhi_LC(bmeshes);
