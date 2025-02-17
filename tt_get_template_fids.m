99eb9f326db9b579c5347382be8f0b89aa5b4201
George O'Neill <g.o'neill@ucl.ac.uk>
2025-02-17 23:34:04 +0000

    Update tt_get_template_fids.m

--- tt_get_template_fids.m
+++ tt_get_template_fids.m
@@ -1,18 +1,23 @@
 function fids = tt_get_template_fids(T)
 
 if nargin == 0
-    T = eye(4)
+    T = eye(4);
 end
 
 [meshes, names] = tt_load_meshes(T);
 
 id = find(contains(names,'torso'));
 
+% % template fiducuals of the torso
+% % - left shoulder   (pt 901)
+% % - right shoulder  (pt 1953)
+% % - low spine       (pt 1147)
+% 
+% fids = meshes{id}.vertices([901 1953 1147],:);
+
 % template fiducuals of the torso
-% - left shoulder   (pt 901)
-% - right shoulder  (pt 1953)
-% - low spine       (pt 1147)
+% - left shoulder   (pt 1)
+% - right shoulder  (pt 3158)
+% - low spine       (pt 1659)
 
-fids = meshes{id}.vertices([901 1953 1147],:);
-
-
+fids = meshes{id}.vertices([1 3158 1659],:);
